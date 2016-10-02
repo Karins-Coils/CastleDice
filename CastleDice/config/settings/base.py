1497f92455d9d6199672cf293c46e21a8d3711f3
Kelsey Hawley <k.karin.hawley@gmail.com>
2016-10-02 12:58:04 -0700

    splitting out debug and Database settings for local vs prod settings

--- CastleDice/config/settings/base.py
+++ CastleDice/config/settings/base.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'jr6q)wn0l3ix#9_$rg^ifuel#nxg$v!r+l+#$df7c%(ahv+j!g'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
