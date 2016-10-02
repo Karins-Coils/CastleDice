e7bf8d38f82a6d584dd7af6f89fb38ba816a2173
Kelsey Hawley <k.karin.hawley@gmail.com>
2016-10-02 14:40:59 -0700

    moved database stuff for prod to the production.py and it now uses a variable loaded from the ENV

--- CastleDice/config/settings/base.py
+++ CastleDice/config/settings/base.py
@@ -91,20 +91,6 @@ ROOT_URLCONF = 'config.urls'
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'CastleDice',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
--- CastleDice/config/settings/production.py
+++ CastleDice/config/settings/production.py
@@ -1,1 +1,5 @@
+import dj_database_url
+
 from .base import *
+
+DATABASES['default'] = dj_database_url.parse(get_env_var('DATABASE_URL'))
