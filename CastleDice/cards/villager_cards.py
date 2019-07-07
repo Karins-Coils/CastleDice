ab1d36c36a3338af0507ec5c7d86679a3c2b7b08
Karin Hawley <karin@joinhonor.com>
2019-07-06 20:57:50 -0700

    expanding tests and helpers for Deck Cards.  Also adding a couple more cards

--- CastleDice/cards/villager_cards.py
+++ CastleDice/cards/villager_cards.py
@@ -1,11 +1,13 @@
 from .base_classes import BaseCard
-from .base_classes import VillagerDeckMixin
-from .base_classes import NormalDiscardMixin
+from .base_classes import GatherPhaseMixin
 from .base_classes import NoBuildMixin
 from .base_classes import NoDescriptionMixin
-from .base_classes import NoScoreMixin
 from .base_classes import NoOngoingMixin
-from .base_classes import GatherPhaseMixin
+from .base_classes import NoScoreMixin
+from .base_classes import NormalDiscardMixin
+from .base_classes import ResourceCost
+from .base_classes import VillagerDeckMixin
+from ..common import Resources
 from ..common import VillagerCards
 
 
@@ -38,7 +40,6 @@ class Soldier(
 
 class Worker(
     GatherPhaseMixin,
-    NoBuildMixin,
     NoDescriptionMixin,
     NoOngoingMixin,
     NormalDiscardMixin,
@@ -49,8 +50,40 @@ class Worker(
     _constant = VillagerCards.WORKER
 
     @property
-    def description(self):
-        return None
+    def build_cost(self):
+        return (
+            ResourceCost(Resources.WOOD, 1),
+            ResourceCost(Resources.STONE, 2),
+        )
+
+    def is_playable(self):
+        # if current phase is playable phase return True
+        raise NotImplementedError()
+
+    def play(self):
+        # confirm is_playable
+        # reroll player's barbarians
+        # keep any that were barbarians again
+        raise NotImplementedError()
+
+
+class Farmer(
+    GatherPhaseMixin,
+    NoDescriptionMixin,
+    NoOngoingMixin,
+    NormalDiscardMixin,
+    NoScoreMixin,
+    VillagerDeckMixin,
+    BaseCard
+):
+    _constant = VillagerCards.FARMER
+
+    @property
+    def build_cost(self):
+        return (
+            ResourceCost(Resources.WOOD, 2),
+            ResourceCost(Resources.LAND, 1),
+        )
 
     def is_playable(self):
         # if current phase is playable phase return True
--- CastleDice/cards/villager_cards_test.py
+++ CastleDice/cards/villager_cards_test.py
@@ -1,19 +1,42 @@
 import unittest
 
+from .villager_cards import Farmer
 from .villager_cards import Soldier
 from .villager_cards import Worker
-from ..common import VillagerCards
 
 
-class SoldierTest(unittest.TestCase):
-    def test_success(self):
-        self.assertTrue(Soldier())
+class CardTestBase(unittest.TestCase):
+    _card = None
+
+    def setUp(self):
+        self.card = self._card()
+
+    def has_description(self):
+        self.assertTrue(self.card.description)
+
+    def has_no_description(self):
+        self.assertFalse(self.card.description)
+
+    def has_build_cost(self):
+        self.assertTrue(self.card.build_cost)
+
+    def has_no_build_cost(self):
+        self.assertFalse(self.card.build_cost)
+        self.assertEqual(len(self.card.build_cost), 0)
+
+    def _test_base_properties(self):
+        self.assertTrue(self.card)
+        self.assertTrue(self.card.card_id)
+        self.assertTrue(self.card.name)
+
+
+class SoldierTest(CardTestBase):
+    _card = Soldier
 
     def test_properties(self):
-        soldier = Soldier()
-        self.assertEqual(soldier.card_id, VillagerCards.SOLDIER)
-        self.assertEqual(soldier.name, VillagerCards.SOLDIER.name)
-        self.assertTrue(soldier.description)
+        self._test_base_properties()
+        self.has_description()
+        self.has_no_build_cost()
 
     @unittest.skip
     def test_play(self):
@@ -28,15 +51,34 @@ class SoldierTest(unittest.TestCase):
         raise NotImplementedError()
 
 
-class WorkerTest(unittest.TestCase):
-    def test_success(self):
-        self.assertTrue(Worker())
+class WorkerTest(CardTestBase):
+    _card = Worker
 
     def test_properties(self):
-        worker = Worker()
-        self.assertEqual(worker.card_id, VillagerCards.WORKER)
-        self.assertEqual(worker.name, VillagerCards.WORKER.name)
-        self.assertTrue(worker.description)
+        self._test_base_properties()
+        self.has_no_description()
+        self.has_build_cost()
+
+    @unittest.skip
+    def test_play(self):
+        raise NotImplementedError()
+
+    @unittest.skip
+    def test_is_playable_true(self):
+        raise NotImplementedError()
+
+    @unittest.skip
+    def test_is_playable_false(self):
+        raise NotImplementedError()
+
+
+class FarmerTest(CardTestBase):
+    _card = Farmer
+
+    def test_properties(self):
+        self._test_base_properties()
+        self.has_no_description()
+        self.has_build_cost()
 
     @unittest.skip
     def test_play(self):
