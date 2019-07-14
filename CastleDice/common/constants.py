742ce12b8ca205af76408c17ead59243d7a1d1b5
Karin Hawley <karin@joinhonor.com>
2019-07-13 21:07:55 -0700

    moving non constant based common static variables to setup, which is a better thematic fit

--- CastleDice/common/constants.py
+++ CastleDice/common/constants.py
@@ -16,9 +16,6 @@ __all__ = [
     'TurnType',
     'VillagerCardType',
     'VillagerType',
-    'JOAN',
-    'JOAN_GATHER_PREFERENCE',
-    'PHASE',
 ]
 
 
@@ -191,38 +188,3 @@ class TurnType(Constants):
     FIFTH = 5
     SIXTH = 6
     SEVENTH = 7
-
-
-# -- SOLO Play constants -- #
-# not really a groupable type.  Joan is just... Joan...
-JOAN = 'joan'
-
-# Joan's Preferences, with first element being her first choice
-JOAN_GATHER_PREFERENCE = ResourceType.values() + AnimalType.values()
-
-
-# -- Game Data -- #
-PHASE = (
-    # skip zero index for ease of calculation
-    (),
-    # 1 - Horse
-    ("Player Order", "Determine who goes first"),
-    # 2 - Draw phase, Chicken
-    ("Choose Cards", "Choose & Draw cards in order (clockwise)"),
-    # 3 - Choose, Cow
-    ("Choose Dice", "Players pick Choice dice (clockwise)"),
-    # 4 - World Pool
-    ("Roll Dice", "All dice are rolled; World Formed"),
-    # 5 - Gather, Pig
-    ("Gather Dice", "Gather resources in order until gone (clockwise)"),
-    # 6 - Market
-    ("Go to Market", "Go to Market (clockwise)"),
-    # 7 - Workers
-    ("Workers Produce", "Workers Produce"),
-    # 8 - Merchants
-    ("Merchants Trade", "Merchants Trade"),
-    # 9 - Build
-    ("Build", "Build"),
-    # 10 -Barbarians
-    ("Barbarians Raid", "Barbarians Raid"),
-)
