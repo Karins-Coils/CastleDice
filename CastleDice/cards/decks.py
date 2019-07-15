0a8dc4009950b5e1e5e2045583ab6e7370f21cea
Karin Hawley <karin@joinhonor.com>
2019-07-14 21:03:20 -0700

    adding type check when instantiating a draw/discard pile with a deck

--- CastleDice/cards/decks.py
+++ CastleDice/cards/decks.py
@@ -1,6 +1,9 @@
+from typing import ClassVar
 from typing import Dict
-from typing import Sequence
+from typing import NoReturn
 from typing import Optional
+from typing import Sequence
+from typing import Type
 from typing import Union
 
 from CastleDice.common.constants import CastleCardType
@@ -8,6 +11,9 @@ from CastleDice.common.constants import DeckName
 from CastleDice.common.constants import MarketCardType
 from CastleDice.common.constants import VillagerCardType
 from .castle_cards import CastleCard
+from .exceptions import InvalidCastleCardTypeError
+from .exceptions import InvalidMarketCardTypeError
+from .exceptions import InvalidVillagerCardTypeError
 from .market_cards import MarketCard
 from .villager_cards import VillagerCard
 
@@ -24,8 +30,10 @@ _card_list_type = Sequence[_card_type]
 
 class _Deck(object):
     # required on child class
-    _deck_type: DeckName = None
-    _deck_makeup: Dict[_card_type, int] = None
+    _deck_type: ClassVar[DeckName] = None
+    _deck_makeup: ClassVar[Dict[_card_type, int]] = None
+    _card_type: ClassVar[Type[_card_type]] = None
+    _card_type_error: ClassVar[Type[Exception]] = None
 
     # instance specific attributes
     _draw_pile: Optional[_card_list_type] = []
@@ -35,8 +43,25 @@ class _Deck(object):
             self,
             draw_pile: Optional[_card_list_type] = None,
             discard_pile: Optional[_card_list_type] = None):
-        self._draw_pile = draw_pile
-        self._discard_pile = discard_pile
+        self._draw_pile = draw_pile or []
+        self._discard_pile = discard_pile or []
+
+        self._confirm_card_types_in_pile(self._draw_pile)
+        self._confirm_card_types_in_pile(self._discard_pile)
+
+    def _confirm_card_types_in_pile(self, pile: _card_list_type) -> NoReturn:
+        """Confirms all cards in the list are the expected type, or raises error
+
+        :param pile: list of draw/discard cards to check
+        :type: _card_list_type
+        :raises Union[InvalidCastleCardTypeError, InvalidMarketCardTypeError,
+            InvalidVillagerCardTypeError]: when any of the pile are not the expected type
+        """
+        for card in pile:
+            if card.card_id not in self._card_type:
+                raise self._card_type_error("Unexpected card type %s for %s" % (
+                    card, self.__class__
+                ))
 
     @property
     def type(self) -> DeckName:
@@ -45,6 +70,8 @@ class _Deck(object):
 
 class CastleDeck(_Deck):
     _deck_type = DeckName.CASTLE
+    _card_type = CastleCardType
+    _card_type_error = InvalidCastleCardTypeError
     _deck_makeup = {
         CastleCardType.ADVISOR: 2,
         CastleCardType.ALCHEMIST: 1,
@@ -67,6 +94,8 @@ class CastleDeck(_Deck):
 
 class MarketDeck(_Deck):
     _deck_type = DeckName.MARKET
+    _card_type = MarketCardType
+    _card_type_error = InvalidMarketCardTypeError
     _deck_makeup = {
         MarketCardType.BARD: 3,
         MarketCardType.HUNGRY_BARBARIANS: 3,
@@ -79,6 +108,8 @@ class MarketDeck(_Deck):
 
 class VillagerDeck(_Deck):
     _deck_type = DeckName.VILLAGER
+    _card_type = VillagerCardType
+    _card_type_error = InvalidVillagerCardTypeError
     _deck_makeup = {
         VillagerCardType.FARMER: 7,
         VillagerCardType.GUARD: 7,
--- CastleDice/cards/tests/test_decks.py
+++ CastleDice/cards/tests/test_decks.py
@@ -1,8 +1,12 @@
 import unittest
+from typing import ClassVar
 from typing import Union
+from typing import Type
 
+from CastleDice.cards.castle_cards import CastleCard
 from CastleDice.cards.decks import MarketDeck
 from CastleDice.cards.decks import VillagerDeck
+from CastleDice.cards.villager_cards import VillagerCard
 from CastleDice.common.constants import CastleCardType
 from CastleDice.common.constants import DeckName
 from CastleDice.common.constants import MarketCardType
@@ -16,14 +20,16 @@ skip_test_if_base_class = create_skip_test_if_base_class_decorator('deck_class')
 
 
 class DeckBaseTest(unittest.TestCase):
-    deck_class: _Deck = None
-    deck_type: DeckName = None
-    card_type: Union[CastleCardType, MarketCardType, VillagerCardType] = None
+    deck_class: ClassVar[Type[_Deck]] = None
+    deck_type: ClassVar[DeckName] = None
+    card_type: ClassVar[Union[CastleCardType, MarketCardType, VillagerCardType]] = None
 
     @skip_test_if_base_class
     def test_attributes(self):
         self.assertEqual(self.deck_class().type, self.deck_type)
         self.assertEqual(self.deck_class._deck_type, self.deck_type)
+        self.assertEqual(self.deck_class._card_type, self.card_type)
+        self.assertIsNotNone(self.deck_class._card_type_error)
 
     @skip_test_if_base_class
     def test_makeup_has_all_cards(self):
@@ -41,6 +47,31 @@ class _DeckTest(unittest.TestCase):
         }
 
         self.assertCountEqual(decks.keys(), DeckName.values())
+
+    def test_initialized_empty(self):
+        deck = _Deck()
+        self.assertEqual(deck._discard_pile, [])
+        self.assertEqual(deck._draw_pile, [])
+
+    def test_initialized_with_piles(self):
+        draw_pile = [CastleCard(CastleCardType.ADVISOR), CastleCard(CastleCardType.SQUIRE)]
+        discard_pile = [CastleCard(CastleCardType.DEEP_MOAT),
+                        CastleCard(CastleCardType.DAUGHTER)]
+        deck = CastleDeck(draw_pile, discard_pile)
+        self.assertEqual(deck._discard_pile, discard_pile)
+        self.assertEqual(deck._draw_pile, draw_pile)
+
+    def test_initialized_with_bad_draw_pile(self):
+        draw_pile = [VillagerCard(VillagerCardType.WORKER), CastleCard(CastleCardType.SQUIRE)]
+        discard_pile = []
+        with self.assertRaises(CastleDeck._card_type_error):
+            CastleDeck(draw_pile, discard_pile)
+
+    def test_initialized_with_bad_discard_pile(self):
+        draw_pile = []
+        discard_pile = [VillagerCard(VillagerCardType.WORKER), CastleCard(CastleCardType.SQUIRE)]
+        with self.assertRaises(CastleDeck._card_type_error):
+            CastleDeck(draw_pile, discard_pile)
 
 
 class CastleDeckTest(DeckBaseTest):
