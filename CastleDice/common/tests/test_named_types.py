79d165014e499a3f2cb5a2ae929bb2b5e48b5228
Karin Hawley <karin@joinhonor.com>
2019-07-22 20:09:14 -0700

    rearranging assertions to be (expected, actual) in order

--- CastleDice/common/tests/test_named_types.py
+++ CastleDice/common/tests/test_named_types.py
@@ -1,4 +1,7 @@
 import unittest
+from typing import Any
+from typing import Iterable
+from typing import Tuple
 
 from ..named_types import NamedFloat
 from ..named_types import NamedInt
@@ -9,9 +12,9 @@ from ..named_types import create_named_type
 
 
 class BaseTest(unittest.TestCase):
-    def _confirm_assertions(self, assertions):
+    def _confirm_assertions(self, assertions: Iterable[Tuple[Any, Any]]):
         """
-        :param assertions list(tuple): tuples - (expected, actual)
+        :param assertions: tuples - (expected, actual)
         """
         for idx, assertion in enumerate(assertions):
             with self.subTest(i=idx):
@@ -32,30 +35,30 @@ class TestNamedTypesInstanceOf(BaseTest):
 
     def test_instance_of_NamedType(self):
         assertions = [
-            # (actual, expected)
-            (isinstance(NamedStr('x', 'a'), NamedType), True),
-            (isinstance(NamedFloat('x', 1.3), NamedType), True),
-            (isinstance(NamedInt('x', 5), NamedType), True),
+            # (expected, actual)
+            (True, isinstance(NamedStr('x', 'a'), NamedType)),
+            (True, isinstance(NamedFloat('x', 1.3), NamedType)),
+            (True, isinstance(NamedInt('x', 5), NamedType)),
         ]
 
         self._confirm_assertions(assertions)
 
     def test_instance_of_self(self):
         assertions = [
-            # (actual, expected)
-            (isinstance(NamedStr('x', 'a'), NamedStr), True),
-            (isinstance(NamedFloat('x', 1.3), NamedFloat), True),
-            (isinstance(NamedInt('x', 5), NamedInt), True),
+            # (expected, actual)
+            (True, isinstance(NamedStr('x', 'a'), NamedStr)),
+            (True, isinstance(NamedFloat('x', 1.3), NamedFloat)),
+            (True, isinstance(NamedInt('x', 5), NamedInt)),
         ]
 
         self._confirm_assertions(assertions)
 
     def test_not_instances_of_other_types(self):
         assertions = [
-            # (actual, expected)
-            (isinstance(NamedStr('x', 'a'), int), False),
-            (isinstance(NamedFloat('x', 1.3), int), False),
-            (isinstance(NamedInt('x', 5), str), False),
+            # (expected, actual)
+            (False, isinstance(NamedStr('x', 'a'), int)),
+            (False, isinstance(NamedFloat('x', 1.3), int)),
+            (False, isinstance(NamedInt('x', 5), str)),
         ]
 
         self._confirm_assertions(assertions)
@@ -65,48 +68,48 @@ class TestNamedTypesComparisons(BaseTest):
     def test_compare_equals(self):
         x = 'some_name'
         assertions = [
-            # (actual, expected)
-            (NamedStr(x, 'a') == NamedStr(x, 'a'), True),
-            (NamedStr(x, 'a') == NamedStr(x, 'apple'), False),
-            (NamedStr(x, 'a') != NamedStr(x, 'apple'), True),
-            (NamedInt(x, 1) == NamedInt(x, 1), True),
-            (NamedInt(x, 12) == NamedInt(x, 1), False),
-            (NamedInt(x, 12) != NamedInt(x, 1), True),
-            (NamedFloat(x, 1.5) == NamedFloat(x, 1.5), True),
-            (NamedFloat(x, 12.1) == NamedFloat(x, 1.5), False),
-            (NamedFloat(x, 12.1) != NamedFloat(x, 1.5), True),
+            # (expected, actual)
+            (True, NamedStr(x, 'a') == NamedStr(x, 'a')),
+            (False, NamedStr(x, 'a') == NamedStr(x, 'apple')),
+            (True, NamedStr(x, 'a') != NamedStr(x, 'apple')),
+            (True, NamedInt(x, 1) == NamedInt(x, 1)),
+            (False, NamedInt(x, 12) == NamedInt(x, 1)),
+            (True, NamedInt(x, 12) != NamedInt(x, 1)),
+            (True, NamedFloat(x, 1.5) == NamedFloat(x, 1.5)),
+            (False, NamedFloat(x, 12.1) == NamedFloat(x, 1.5)),
+            (True, NamedFloat(x, 12.1) != NamedFloat(x, 1.5)),
         ]
         self._confirm_assertions(assertions)
 
     def test_compare_greater_less_than(self):
         x = 'some_name'
         assertions = [
-            # (actual, expected)
-            (NamedStr(x, 'a') < NamedStr(x, 'a'), False),
-            (NamedStr(x, 'a') < NamedStr(x, 'apple'), True),
-            (NamedStr(x, 'a') > NamedStr(x, 'apple'), False),
-            (NamedInt(x, 1) < NamedInt(x, 1), False),
-            (NamedInt(x, 12) < NamedInt(x, 1), False),
-            (NamedInt(x, 12) > NamedInt(x, 1), True),
-            (NamedFloat(x, 1.5) < NamedFloat(x, 1.5), False),
-            (NamedFloat(x, 12.1) < NamedFloat(x, 1.5), False),
-            (NamedFloat(x, 12.1) > NamedFloat(x, 1.5), True),
+            # (expected, actual)
+            (False, NamedStr(x, 'a') < NamedStr(x, 'a')),
+            (True, NamedStr(x, 'a') < NamedStr(x, 'apple')),
+            (False, NamedStr(x, 'a') > NamedStr(x, 'apple')),
+            (False, NamedInt(x, 1) < NamedInt(x, 1)),
+            (False, NamedInt(x, 12) < NamedInt(x, 1)),
+            (True, NamedInt(x, 12) > NamedInt(x, 1)),
+            (False, NamedFloat(x, 1.5) < NamedFloat(x, 1.5)),
+            (False, NamedFloat(x, 12.1) < NamedFloat(x, 1.5)),
+            (True, NamedFloat(x, 12.1) > NamedFloat(x, 1.5)),
         ]
         self._confirm_assertions(assertions)
 
     def test_compare_greater_less_than_or_equal(self):
         x = 'some_name'
         assertions = [
-            # (actual, expected)
-            (NamedStr(x, 'a') <= NamedStr(x, 'a'), True),
-            (NamedStr(x, 'a') <= NamedStr(x, 'apple'), True),
-            (NamedStr(x, 'a') >= NamedStr(x, 'apple'), False),
-            (NamedInt(x, 1) <= NamedInt(x, 1), True),
-            (NamedInt(x, 12) <= NamedInt(x, 1), False),
-            (NamedInt(x, 12) >= NamedInt(x, 1), True),
-            (NamedFloat(x, 1.5) <= NamedFloat(x, 1.5), True),
-            (NamedFloat(x, 12.1) <= NamedFloat(x, 1.5), False),
-            (NamedFloat(x, 12.1) >= NamedFloat(x, 1.5), True),
+            # (expected, actual)
+            (True, NamedStr(x, 'a') <= NamedStr(x, 'a')),
+            (True, NamedStr(x, 'a') <= NamedStr(x, 'apple')),
+            (False, NamedStr(x, 'a') >= NamedStr(x, 'apple')),
+            (True, NamedInt(x, 1) <= NamedInt(x, 1)),
+            (False, NamedInt(x, 12) <= NamedInt(x, 1)),
+            (True, NamedInt(x, 12) >= NamedInt(x, 1)),
+            (True, NamedFloat(x, 1.5) <= NamedFloat(x, 1.5)),
+            (False, NamedFloat(x, 12.1) <= NamedFloat(x, 1.5)),
+            (True, NamedFloat(x, 12.1) >= NamedFloat(x, 1.5)),
         ]
         self._confirm_assertions(assertions)
 
@@ -114,29 +117,29 @@ class TestNamedTypesComparisons(BaseTest):
         # id(float) < (id(int)) < id(str)
         x = 'some_name'
         assertions = [
-            # (actual, expected)
-            (NamedStr(x, 'a') <= NamedInt(x, 1), False),
-            (NamedStr(x, 'a') > NamedInt(x, 1), True),
-            (NamedStr(x, 'a') == NamedInt(x, 1), False),
-            (NamedStr(x, 'a') <= NamedFloat(x, 1.5), False),
-            (NamedStr(x, 'a') >= NamedFloat(x, 1.5), True),
-            (NamedStr(x, 'a') != NamedFloat(x, 1.5), True),
-            (NamedInt(x, 1) <= NamedFloat(x, 1.5), True),
-            (NamedInt(x, 1) > NamedFloat(x, 1.5), False),
-            (NamedInt(x, 1) == NamedFloat(x, 1.5), False),
+            # (expected, actual)
+            (False, NamedStr(x, 'a') <= NamedInt(x, 1)),
+            (True, NamedStr(x, 'a') > NamedInt(x, 1)),
+            (False, NamedStr(x, 'a') == NamedInt(x, 1)),
+            (False, NamedStr(x, 'a') <= NamedFloat(x, 1.5)),
+            (True, NamedStr(x, 'a') >= NamedFloat(x, 1.5)),
+            (True, NamedStr(x, 'a') != NamedFloat(x, 1.5)),
+            (True, NamedInt(x, 1) <= NamedFloat(x, 1.5)),
+            (False, NamedInt(x, 1) > NamedFloat(x, 1.5)),
+            (False, NamedInt(x, 1) == NamedFloat(x, 1.5)),
         ]
         self._confirm_assertions(assertions)
 
     def test_keep_float_int_comparisons(self):
         x = 'some_name'
         assertions = [
-            # (actual, expected)
-            (NamedFloat(x, 1.0) == NamedInt(x, 1), True),
-            (NamedFloat(x, 1.5) > NamedInt(x, 1), True),
-            (NamedFloat(x, 1.2) != NamedInt(x, 1), True),
-            (NamedInt(x, 1) <= NamedFloat(x, 1.5), True),
-            (NamedInt(x, 1) >= NamedFloat(x, 1.5), False),
-            (NamedInt(x, 1) < NamedFloat(x, 1.5), True),
+            # (expected, actual)
+            (True, NamedFloat(x, 1.0) == NamedInt(x, 1)),
+            (True, NamedFloat(x, 1.5) > NamedInt(x, 1)),
+            (True, NamedFloat(x, 1.2) != NamedInt(x, 1)),
+            (True, NamedInt(x, 1) <= NamedFloat(x, 1.5)),
+            (False, NamedInt(x, 1) >= NamedFloat(x, 1.5)),
+            (True, NamedInt(x, 1) < NamedFloat(x, 1.5)),
         ]
         self._confirm_assertions(assertions)
 
@@ -144,10 +147,10 @@ class TestNamedTypesComparisons(BaseTest):
 class TestNamedTypeCreation(BaseTest):
     def test_get_existing_named_types(self):
         assertions = [
-            # (actual, expected)
-            (create_named_type(float), NamedFloat),
-            (create_named_type(int), NamedInt),
-            (create_named_type(str), NamedStr),
+            # (expected, actual)
+            (NamedFloat, create_named_type(float)),
+            (NamedInt, create_named_type(int)),
+            (NamedStr, create_named_type(str)),
         ]
         self._confirm_assertions(assertions)
 
