d75e00bc9fee2eb5828e55136426dba429e32318
Karin Hawley <karin@joinhonor.com>
2019-11-28 11:47:08 -0800

    add test settings, connected to a CastleDiceTest db

---
+++ config/settings/test.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DEBUG = True
+
+# don't want emails actually firing off during local dev!
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# default local DB setup using postgres
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "CastleDiceTest",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
