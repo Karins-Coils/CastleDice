1bbec1f1c1a99dd56d501b59ac9f6306686bad5d
K Hawley <karin@karinscoils.com>
2021-09-30 23:24:19 -0700

    pulling in cookie-cutter urls
    likely a bug here, as I haven't pulled in the users module, and haven't renamed my app dir from CastleDice to castledice

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views import defaults as default_views
+from django.views.generic import TemplateView
 
 from CastleDice.game.views import (
     HomeView,
@@ -17,6 +21,17 @@ from CastleDice.die.views import ChooseDiceView, RollDiceView, GatherDiceView
 admin.autodiscover()
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+    ),
+    # Django Admin, use {% url 'admin:index' %}
+    path(settings.ADMIN_URL, admin.site.urls),
+    # User management
+    path("users/", include("castledice.users.urls", namespace="users")),
+    path("accounts/", include("allauth.urls")),
+    # Your stuff: custom urls includes go here
+
     # Examples:
     # url(r'^$', 'CastleDice.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -42,5 +57,32 @@ urlpatterns = [
     path("game_<int:game_id>/waiting", WaitingView.as_view(), name="waiting"),
     # pass view, to temporary advance through missing phases
     path("game_<int:game_id>/pass_phase", PassPhaseView.as_view(), name="pass_phase"),
-    path("porkchop/", admin.site.urls),
-]
+    # path("porkchop/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
